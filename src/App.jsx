5a6d550f01dcf44dc539e0f83b82d203b69e9f48
gustavoweb <contato@gustavoweb.com>
2025-10-30 08:14:32 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ function App() {
 
     // Try Executa os comandos
     try{
-      const API_KEY = "";
-      const url = "";
+      const API_KEY = "50878f4678cd0841144b44b2fca0ccc0";
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API_KEY}&units=metric&lang=pt_br`;
       const resposta = await fetch(url);
 
       if(!resposta.ok){
